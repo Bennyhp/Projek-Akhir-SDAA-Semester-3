2dc61f144c0bdf3a3dc54eedb1ba50b3c511b64a
Bennyhparh <74233673+Bennyhp@users.noreply.github.com>
2021-11-29 21:23:25 +0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include"LogAdmin.cpp"
+#include"Pembeli.cpp"
+
+
+using namespace std;
+
+
+int pilihan;
+
+int main()
+{
+	while(pilihan != 3)
+	{
+		system("CLS");
+		cout<< "Selamat Datang Di Toko Sepatu" <<endl;
+		cout<< "1. Masuk Sebagai Admin"<<endl;
+		cout<< "2. Masuk Sebagai Pembeli"<<endl;
+		cout<< "3. Keluar"<<endl;
+		cout<< "Masukan Pilihan Anda :";cin>>pilihan;
+		switch(pilihan)
+		{
+			case 1:
+				inAdmin();
+				break;
+			case 2:
+				menuPembeli();
+				break;
+			case 3:
+				continue;
+			default :
+				cout<<"Pilihan tidak ada"<<endl;
+				tekanLanjut();
+		}
+	}
+}
